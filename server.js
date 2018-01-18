c9e7846149901c081128e03e62bc8e0cb01bf4fd
jfsuarez <jsuarez@optecom.com.co>
2018-01-18 17:42:05 -0500

    ult-cambio
    ult-cambio

--- server.js
+++ server.js
@@ -1,28 +1,62 @@
 //  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
+// var express = require('express'),
+//     app     = express(),
+//     morgan  = require('morgan');
     
-Object.assign=require('object-assign')
+// Object.assign=require('object-assign')
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+// app.engine('html', require('ejs').renderFile);
+// app.use(morgan('combined'))
+
+// var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+//     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+// // error handling
+// app.use(function(err, req, res, next){
+//   console.error(err.stack);
+//   res.status(500).send('Something bad happened!');
+// });
+
+// var io = require('socket.io').listen(app); //SocketIO - Seccion 1
+
+// app.listen(port, ip);
+// console.log('Server running on http://%s:%s', ip, port);
+
+// //SocketIO - Seccion 2
+// io.set("origins", "*:*");
+
+// var currentPrice = [{'id':1,'msj':'Welcome aboard!','owner':'SIT'}];
+
+
+// io.on('connection', function (socket) {
+//  socket.emit('priceUpdate',currentPrice);
+//  socket.on('bid', function (data) {
+//    currentPrice = data;
+//    socket.emit('priceUpdate',currentPrice);
+//    socket.broadcast.emit('priceUpdate',currentPrice);
+//  });
+
+// });
+
+// console.log('current price:'+currentPrice);
+
+// module.exports = app ;
+// */
+
+var http = require('http');
+var express = require('express'),
+    app = module.exports.app = express();
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);  //pass a http.Server instance
+// server.listen(80);  //listen on port 80
 
-var io = require('socket.io').listen(app); //SocketIO - Seccion 1
-
-app.listen(port, ip);
+server.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
-//SocketIO - Seccion 2
 io.set("origins", "*:*");
 
 var currentPrice = [{'id':1,'msj':'Welcome aboard!','owner':'SIT'}];
@@ -39,5 +73,3 @@ io.on('connection', function (socket) {
 });
 
 console.log('current price:'+currentPrice);
-
-module.exports = app ;
